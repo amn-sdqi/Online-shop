b0a4f4b37940bfdbc0febcc426c2513db6b385d0
amn-sdqi <amaanshahid2510@gmail.com>
2024-04-06 23:38:40 +0530

    image preview update(not working)

--- models/product.model.js
+++ models/product.model.js
@@ -1,38 +1,91 @@
-const db = require('../data/database');
+const mongodb = require("mongodb");
+
+const db = require("../data/database");
 
 class Product {
-  constructor(productData) {
-    this.title = productData.title;
-    this.summary = productData.summary;
-    this.price = +productData.price;
-    this.description = productData.description;
-    this.image = productData.image; // the name of the image file
-    this.imagePath = `product-data/images/${productData.image}`;
-    this.imageUrl = `/products/assets/images/${productData.image}`;
-    if (productData._id) {
-      this.id = productData._id.toString();
-    }
-  }
+	constructor(productData) {
+		this.title = productData.title;
+		this.summary = productData.summary;
+		this.price = +productData.price;
+		this.description = productData.description;
+		this.image = productData.image; // the name of the image file
+		this.updateImageData();
+		if (productData._id) {
+			this.id = productData._id.toString();
+		}
+	}
 
-  static async findAll() {
-    const products = await db.getDb().collection('products').find().toArray();
+	static async findById(productId) {
+		let prodId;
+		try {
+			prodId = new mongodb.ObjectId(productId);
+		} catch (error) {
+			error.code = 404;
+			throw error;
+		}
+		const product = await db
+			.getDb()
+			.collection("products")
+			.findOne({ _id: prodId });
 
-    return products.map(function(productDocument) {
-      return new Product(productDocument);
-    });
-  }
+		if (!product) {
+			const error = new Error("Could not find product with provided id.");
+			error.code = 404;
+			throw error;
+		}
 
-  async save() {
-    const productData = {
-      title: this.title,
-      summary: this.summary,
-      price: this.price,
-      description: this.description,
-      image: this.image
-    };
+		return new Product(product);
+	}
 
-    await db.getDb().collection('products').insertOne(productData);
-  }
+	static async findAll() {
+		const products = await db.getDb().collection("products").find().toArray();
+
+		return products.map(function (productDocument) {
+			return new Product(productDocument);
+		});
+	}
+
+	updateImageData() {
+		this.imagePath = `product-data/images/${this.image}`;
+		this.imageUrl = `/products/assets/images/${this.image}`;
+	}
+
+	async save() {
+		const productData = {
+			title: this.title,
+			summary: this.summary,
+			price: this.price,
+			description: this.description,
+			image: this.image,
+		};
+
+		if (this.id) {
+			const productId = new mongodb.ObjectId(this.id);
+
+			if (!this.image) {
+				delete productData.image;
+			}
+
+			await db.getDb().collection("products").updateOne(
+				{ _id: productId },
+				{
+					$set: productData,
+				}
+			);
+		} else {
+			await db.getDb().collection("products").insertOne(productData);
+		}
+	}
+
+	replaceImage(newImage) {
+		this.image = newImage;
+		this.updateImageData();
+	}
+
+	remove() {
+		const productId = new mongodb.ObjectId(this.id);
+		return db.getDb.collection("products").deleteOne({ _id: productId });
+	}
 }
 
 module.exports = Product;
